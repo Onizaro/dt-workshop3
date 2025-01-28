1c188167992085426e4327bf9b6fd84ee6981182
Anton Nomed <anton.nomed@edu.devinci.fr>
2025-01-28 16:29:21 +0100

    Ajout de label_encoders

--- anton_train.py
+++ anton_train.py
@@ -14,6 +14,13 @@ def load_and_prepare_data():
     y = data["Survived"]
     return train_test_split(X, y, test_size=0.2, random_state=42)
 
+# Encodage des variables catégoriques
+label_encoders = {}
+for col in ["sex", "class", "embarked"]:
+    le = LabelEncoder()
+    data[col] = le.fit_transform(data[col])
+    label_encoders[col] = le
+
 # Entraîner le modèle
 X_train, X_test, y_train, y_test = load_and_prepare_data()
 model = LinearRegression()
